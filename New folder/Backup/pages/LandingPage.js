9afd161ccd38ccae51a4109c602e48c164c4c278
JinFengAng <81544598+JinFengAng@users.noreply.github.com>
2021-10-04 10:03:20 +0800

    Delete New folder directory

--- New folder/Backup/pages/LandingPage.js
+++
@@ -1,9 +0,0 @@
-import { Redirect } from "react-router-dom";
-
-export default function Landing() {
-    return (
-        // Currently, we do not need a landing page. This will be added in when necessary.
-        // For now, we simply redirect to the login page.
-        <Redirect to='/admin' />
-    )
-}
